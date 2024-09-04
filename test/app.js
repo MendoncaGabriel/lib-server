d1b08e7c12df748496dc141237d1216c082a0782
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2024-09-04 19:56:00 -0400

    init branch

--- test/app.js
+++ test/app.js
@@ -5,13 +5,15 @@ const app = new Server()
 app.use(app.json())
 
 app.get("/", (req, res) => {
-    res
+    
     // res.send("Gabriel")
     // res.send({nome: "Gabriel"})
     // res.send(path.resolve("public", "img.webp"))
     res.send(path.resolve("public", "video.mp4"))
     // res.send(`<p style="color:red;">teste</p>`)
 })
+
+app.use("/teste", (req, res) => res.send("teste"))
 
 app.listen(3000, () => {
     console.log("Servidor esta rodando")
